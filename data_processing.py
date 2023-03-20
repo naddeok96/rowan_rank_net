3d7192de91656706230f23abae724991af3114dc
Kyle Naddeo <naddeok96@github.com>
2023-03-20 13:27:18 -0400

    changed to a func that returns pandas df and torch tensor

--- data_processing.py
+++ data_processing.py
@@ -1,7 +1,18 @@
-import torch
 import pandas as pd
+import torch
 
-def process_data(filename):
+def process_data(filename: str) -> tuple:
+    """
+    Read an Excel file, preprocess the data, and convert it to a PyTorch tensor.
+
+    Args:
+        filename (str): The name of the Excel file to read.
+
+    Returns:
+        A tuple containing two objects:
+        1. A Pandas DataFrame with the preprocessed data.
+        2. A PyTorch tensor with the preprocessed data.
+    """
     # Read the Excel file and set the fourth row as header
     df = pd.read_excel(filename, header=4)
 
@@ -13,14 +24,22 @@ def process_data(filename):
 
     # Convert NaNs and "N\A"s to None
     df = df.replace({"NaN": None, "N\A": None})
-
+    
+    # Replace non-numeric characters with empty strings
+    df = df.replace('[^0-9]', '', regex=True)
+    
     # Convert all integers and strings to floats
-    df = df.apply(lambda x: pd.to_numeric(x, errors='coerce'))
+    df = df.apply(lambda x: pd.to_numeric(x, errors='coerce'))     
 
-    # Remove all occurrences of "$" and "%" from string elements
-    for col in df.columns:
-        if df[col].dtype == object:
-            df[col] = df[col].apply(lambda x: x.replace('$', '').replace('%', '') if isinstance(x, str) else x)
+    # Replace None values with column averages
+    df = df.fillna(df.mean())
     
     # Convert the Pandas DataFrame to a PyTorch tensor
-    return torch.tensor(df.values, dtype=torch.float32)
+    tensor = torch.tensor(df.values, dtype=torch.float32)
+    
+    return df, tensor
+
+
+if __name__ == "__main__":
+    df, _ = process_data("US News - GB2023Engineering - Embargoed Until 3-29-22.xlsx")
+    df.to_excel("TEMP.xlsx", index=False)
