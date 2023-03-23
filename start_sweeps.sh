a766746d2a71b4198f3b088742f95dd2a5d70cf5
Kyle <naddeok5@rowan.edu>
2023-03-23 15:12:32 -0400

    added starting only two sweeps on one gpu

--- start_sweeps.sh
+++ start_sweeps.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
-# Start four sweeps
-for i in {0..3}; do
-    # Determine the GPU number to use for the current run based on the loop index
-    if [ $((i % 2)) -eq 0 ]; then
-        gpu_number="4"
-    else
-        gpu_number="5"
-    fi
-
-    # Run the kfold_hypersweep.py script with the specified GPU number in the background
+# Start two sweeps
+for i in {0..1}; do
+    # Run the kfold_hypersweep.py script with gpu_number 4 in the background
     # Redirect the output of the script to a file named sweep$i.out
-    nohup python3 kfold_hypersweep.py --gpu_number $gpu_number > "sweep$i.out" &
+    nohup python3 kfold_hypersweep.py --gpu_number 4 > "sweep$i.out" &
 done
 
 # Wait for all sweeps to finish before exiting
 wait
+
+
+# # Start four sweeps
+# for i in {0..3}; do
+#     # Determine the GPU number to use for the current run based on the loop index
+#     if [ $((i % 2)) -eq 0 ]; then
+#         gpu_number="4"
+#     else
+#         gpu_number="5"
+#     fi
+
+#     # Run the kfold_hypersweep.py script with the specified GPU number in the background
+#     # Redirect the output of the script to a file named sweep$i.out
+#     nohup python3 kfold_hypersweep.py --gpu_number $gpu_number > "sweep$i.out" &
+# done
+
+# # Wait for all sweeps to finish before exiting
+# wait
